395dc71986b0a0d4f733d84332d2efc8e1a398b5
Tlstrogen <tlstrogen@gmail.com>
2026-06-22 18:18:05 -0400

    Complete Module 5 API refactor

--- travlr/app.js
+++ travlr/app.js
@@ -3,10 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
 
+// Bring in the database
+require('./app_api/models/db');
+
+// Define routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
@@ -14,34 +20,33 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
+// register handlebars partials
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views', 'partials'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var hbs = require('hbs')
-
+// wire up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
-
-
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views', 'partials'));
-
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-res.locals.message = err.message;
-res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-res.status(err.status || 500);
-res.render('error');
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
